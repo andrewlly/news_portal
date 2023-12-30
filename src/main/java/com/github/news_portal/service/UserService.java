7cd6ee1dcca67ea1588a7ea63465b203804d282e
yul02 <yul02@students.cs.ubc.ca>
2023-12-30 00:31:30 -0800

    login user dao/userDetails done

--- src/main/java/com/github/news_portal/service/UserService.java
+++ src/main/java/com/github/news_portal/service/UserService.java
@@ -2,12 +2,11 @@ package com.github.news_portal.service;
 
 import com.github.news_portal.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.security.core.userdetails.UserDetails;
 
-/**
-* @author liuyu
-* @description 针对表【user】的数据库操作Service
-* @createDate 2023-12-04 23:33:35
-*/
+
 public interface UserService extends IService<User> {
-    public boolean updateUserById(Long uid, User user);
+    boolean updateUserById(Long uid, User user);
+
+    UserDetails loadUserByUsername(String username);
 }
--- src/main/java/com/github/news_portal/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/news_portal/service/impl/UserServiceImpl.java
@@ -1,19 +1,19 @@
 package com.github.news_portal.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.news_portal.domain.User;
+import com.github.news_portal.security.LoginUser;
 import com.github.news_portal.service.UserService;
 import com.github.news_portal.mapper.UserMapper;
 import com.github.news_portal.util.InputParser;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
-/**
-* @author liuyu
-* @description 针对表【user】的数据库操作Service实现
-* @createDate 2023-12-04 23:33:35
-*/
+
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
@@ -33,6 +33,18 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         user.setUid(uid);
         int i = userMapper.updateById(validUser);
         return i != 0;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getUserName, username);
+        User user = userMapper.selectOne(lambdaQueryWrapper);
+        if(Objects.isNull(user)){
+            return null;
+        }
+        //todo: access check
+        return new LoginUser(user);
     }
 }
 
